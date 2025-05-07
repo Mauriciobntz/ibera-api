f491922c05883e4d8d4dcbee2ac6e5500c889a50
Mauriciobntz <mauriciobenitezok@gmail.com>
2025-05-06 21:01:57 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 import mysqlConnection from 'mysql2/promise'
 
 const properties = {
-    host: 'localhost',       // Host de la base de datos
-    user: 'root',          
-    password: '',
-    database: 'ibera'
+    host: 'firebrick-weasel-544564.hostingersite.com',       // Host de la base de datos
+    user: 'u872190053_iberaadmin',          
+    password: 'Bera-1234',
+    database: 'u872190053_ibera'
 }
 
 export const pool = mysqlConnection.createPool(properties);
