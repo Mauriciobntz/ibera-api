6044e0b92aaba1bd711de2a71e5412f7ce5d1b05
Mauriciobntz <mauriciobenitezok@gmail.com>
2025-05-06 21:30:34 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import morgan from 'morgan';
 import { router } from './routes.js';
 
 const app = express();
-
-// Usar el puerto asignado por Render, o 3000 localmente
 app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
@@ -12,5 +10,5 @@ app.use(express.json());
 app.use(router);
 
 app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`);
+  console.log(`Server running on port ${app.get('port')}`);
 });
